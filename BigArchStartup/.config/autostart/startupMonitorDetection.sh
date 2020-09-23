ac5ae4d1c69cf5b8a38a733eebd08065a92cb594
Olav Landmark Pedersen <olavpe@gmail.com>
2020-09-23 12:33:40 +0200

    chaning the startup monitor detection in Trondheim

--- BigArchStartup/.config/autostart/startupMonitorDetection.sh
+++ BigArchStartup/.config/autostart/startupMonitorDetection.sh
@@ -23,7 +23,8 @@ if (xrandr | grep "$EXT disconnected"); then
 else
     echo External monitor detected\n\r
     # For BenQ screen without using laptop
-    # xrandr --output LVDS-0 --off --output DP-1 --primary --mode 3440x1440
+    xrandr --output LVDS-0 --off --output DP-1 --primary --mode 3440x1440
     # For Prediktor office monitor (using both laptop and external)
-    xrandr --output LVDS-0 --primary --auto --right-of DP-1 --auto
+    # xrandr --output LVDS-0 --primary --auto --right-of DP-1 --auto
+    # xrandr --output DP-1 --primary --auto --right-of LVDS-0 --auto
 fi
