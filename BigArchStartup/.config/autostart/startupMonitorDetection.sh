ae5afd32502d44cea0eddf31231ce9a73e18a71f
Olav Landmark Pedersen <olavpe@gmail.com>
2021-01-12 12:02:56 +0100

    Added monitor detection two screens at Lysaker

--- BigArchStartup/.config/autostart/startupMonitorDetection.sh
+++ BigArchStartup/.config/autostart/startupMonitorDetection.sh
@@ -26,8 +26,12 @@ else
     # For BenQ screen without using laptop
     # xrandr --output LVDS-0 --off --output DP-1 --primary --mode 3440x1440
     # For BenQ screen and extra montifor without using laptop
-    xrandr --output LVDS-0 --off --output DP-1 --primary --mode 3440x1440 --output VGA-0 --auto --left-of DP-1
+    # xrandr --output LVDS-0 --off --output DP-1 --primary --mode 3440x1440 --output VGA-0 --auto --left-of DP-1
     # For Prediktor office monitor (using both laptop and external)
     # xrandr --output LVDS-0 --primary --auto --right-of DP-1 --auto
     # xrandr --output DP-1 --primary --auto --right-of LVDS-0 --auto
+    # For home office at Lysaker
+    xrandr --output LVDS-0 --off --output VGA-0 --primary --auto \ 
+    --output HDMI-0 --auto --left-of VGA-0
+
 fi
