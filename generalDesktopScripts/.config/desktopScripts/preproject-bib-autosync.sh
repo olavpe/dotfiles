e078e5dad46f2063779fbb745a9131aad9782d66
Olav Landmark Pedersen <olavpe@gmail.com>
2021-01-16 21:49:26 +0100

    Mendeley sync script works on multiple folders

--- generalDesktopScripts/.config/desktopScripts/preproject-bib-autosync.sh
+++ generalDesktopScripts/.config/desktopScripts/preproject-bib-autosync.sh
@@ -2,6 +2,7 @@
 
 cd $HOME
 while true; do
-  inotifywait -r -e modify,create,delete ~/Documents/BibTex/Olav_Pedersen_PreProject2020.bib
-  rsync -v ~/Documents/BibTex/Olav_Pedersen_PreProject2020.bib ~/gitRepos/Olav_Pedersen_PreProject2020/bibliography.bib 
-done 
+  inotifywait -r -e modify,create,delete ~/Documents/BibTex/
+  rsync -v ~/Documents/BibTex/Olav_Pedersen_PreProject2020.bib ~/gitRepos/Olav_Pedersen_PreProject2020/bibliography.bib
+  rsync -v ~/Documents/BibTex/Olav_Pedersen_Thesis2021.bib ~/gitRepos/Olav_Pedersen_Thesis2021/bibliography.bib
+done
