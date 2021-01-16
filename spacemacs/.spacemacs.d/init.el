59e7087de540a742f8d8ca23796547cfc865aded
Olav Landmark Pedersen <olavpe@gmail.com>
2021-01-16 21:50:24 +0100

    The extra file idea worked for init.el and emacs-custom-settings!!

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -1,2 +1,22 @@
 (load-file "~/.spacemacs.d/spacemacs.el")
 ;; leaving this part so that custom-set-variable might be generated below
+(defun dotspacemacs/emacs-custom-settings ()
+  "Emacs custom settings.
+This is an auto-generated function, do not modify its content directly, use
+Emacs customize menu instead.
+This function is called at the very end of Spacemacs initialization."
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-want-Y-yank-to-eol nil)
+ '(org-agenda-files
+   '("~/.dotfiles/spacemacs/.spacemacs.d/spacemacs.org" "/home/olav/Insync/olavpe/org-agenda/hobby_projects.org" "/home/olav/Insync/olavpe/org-agenda/masters.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+)
