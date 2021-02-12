beffd280f309b8f02cb35a51e5d96abbaeb4ed42
Olav Landmark Pedersen <olavpe@gmail.com>
2021-02-12 18:04:15 +0100

    Changed DTTerminal to router venv

--- generalDesktopScripts/.config/desktopScripts/DTterminal.sh
+++ generalDesktopScripts/.config/desktopScripts/DTterminal.sh
@@ -3,7 +3,7 @@
 # This a script to start the twin in a terminal window
 # in order to quicker get the digital twin service up and running
 
-cd ~/gitRepos/perception-common-library/
+cd ~/gitRepos/perception-router-digitaltwin/
 conda deactivate
 source ./venv/bin/activate
 # dapr run --app-id perception-twin-app --dapr-http-port 3500 --app-port 3001 -- uvicorn --port 3001 twin_service.twin_service:app
