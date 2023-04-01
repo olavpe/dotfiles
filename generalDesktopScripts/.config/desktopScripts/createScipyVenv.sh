a0f1143c85b4d70c0f50f7927b113420a0d1b581
Olav Pedersen <ope@prediktor.no>
2023-04-01 13:43:08 +0200

    Getting PAT to be read as an environment variable instead

--- generalDesktopScripts/.config/desktopScripts/createScipyVenv.sh
+++ generalDesktopScripts/.config/desktopScripts/createScipyVenv.sh
@@ -9,5 +9,5 @@ python3 -m venv venv
 source ./venv/bin/activate
 pip install cython pybind11 pythran numpy
 OPENBLAS=$(brew --prefix openblas) CFLAGS="-falign-functions=8 ${CFLAGS}" pip install --no-use-pep517 scipy
-python -m pip config --site set global.extra-index-url https://PythonFeed:ngdtzvrrbiy25wau7cof6by7kof43j3rp36wozyihylsqj2j3vwq@pkgs.dev.azure.com/PrediktorDevOps/Development/_packaging/PythonFeed/pypi/simple/
+python -m pip config --site set global.extra-index-url https://PythonFeed:$PYTHON_FEED_PAT@pkgs.dev.azure.com/PrediktorDevOps/Development/_packaging/PythonFeed/pypi/simple/
 pip install -r requirements.txt
